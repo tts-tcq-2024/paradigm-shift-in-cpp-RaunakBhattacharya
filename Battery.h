0ffac93d7012e78181e69081b2f6598c32e174af
RaunakBhattacharya <160215825+RaunakBhattacharya@users.noreply.github.com>
2024-07-31 17:52:23 +0530

    Create Battery.h

---
+++ Battery.h
@@ -0,0 +1,33 @@
+#include <string>
+#include <map>
+
+enum class BatteryParameter {
+    TEMPERATURE,
+    SOC,
+    CHARGE_RATE
+};
+
+struct BatteryLimits {
+    float min;
+    float max;
+};
+
+struct BatteryWarningLimits {
+    float min;
+    float max;
+};
+
+class Battery {
+public:
+    Battery();
+    bool isWithinLimits(float value, BatteryLimits limits, BatteryParameter parameter);
+    bool isWithinWarningLimits(float value, BatteryWarningLimits limits, BatteryParameter parameter);
+    bool batteryIsOk(float temperature, float soc, float chargeRate);
+    void testBatteryIsOk();
+private:
+    std::map<BatteryParameter, std::string> parameterErrorMessages;
+    std::map<BatteryParameter, std::string> parameterWarningMessages;
+    std::map<BatteryParameter, std::string> parameterErrorMessagesGerman;
+    std::map<BatteryParameter, std::string> parameterWarningMessagesGerman;
+    std::string language;
+};
